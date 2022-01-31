5043d6163899e0444fe18f68f5ae0c2d519dcce0
michael whalen <michael.whalen@sitechhs.com>
2022-01-30 20:57:57 -0500

    sdf

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
+const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style");
   eleventyConfig.addPassthroughCopy("src/media");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/md");
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
   return {
     dir: {
       input: "src",
